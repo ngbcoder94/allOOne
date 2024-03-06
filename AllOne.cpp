ab0c62fb10e1b1adf977ca2fa9587537b1cb48b3
Nick <burkettng@appstate.edu>
2024-03-06 01:30:42 -0500

    Created class skeleton

---
+++ AllOne.cpp
@@ -0,0 +1,25 @@
+/*
+ * Here is my attempt at the AllOne problem on LeetCode!
+ * Starting with my class AllOne: 
+*/
+class AllOne(){
+
+//Need to create some public data members of my class
+public: 
+	//Here is my constructor for the class
+	AllOne(){
+
+	}
+	
+	//Need to create a increment function
+	void inc(string key){}
+	
+	//Need to also create a decrement function
+	void dcr(string key){}
+
+	//getMax() function for the class
+	string getMaxkey(){}
+	
+	//Then I need to create a getMin() function for the class
+	string getMinKey(){}
+};
